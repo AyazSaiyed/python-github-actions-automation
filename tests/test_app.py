207a50a990bb07b654722c30d3dea2bc03451d50
Ayaz Saiyed <49748587+AyazSaiyed@users.noreply.github.com>
2021-06-23 19:05:43 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import index,index2
 
 
 def test_index():
-    assert index() == "welcome to python github actions demo"
+    assert index() == "welcome to python 1 github actions demo"
     
 
 def test_index2():
