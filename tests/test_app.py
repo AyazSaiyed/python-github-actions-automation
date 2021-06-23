0d48e032e783dc66ce200da917f73ba2c11f5ede
AyazSaiyed <ayazsaiyed.mscit17@gmail.com>
2021-06-23 18:34:50 +0530

    New commit 1

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,7 +2,7 @@ from app import index,index2
 
 
 def test_index():
-    assert index() == "welcome to the demo of python github actions"
+    assert index() == "welcome to the latest updated demo of python github actions"
 
 def test_index2():
 	assert index2() == "Hello, Ayaz!"
